f8db04aa3f1bdaf96a98f6ca5f0ee339bcacc075
daveri <david.eriksson@avanza.se>
2024-04-07 21:55:56 +0200

    Try to fix performance with updates

--- app/celery_tasks.py
+++ app/celery_tasks.py
@@ -1,3 +1,4 @@
+import pymongo
 from celery import shared_task
 
 from app.helper import __send_data_to_channel
@@ -10,7 +11,8 @@ from channels.layers import get_channel_layer
 def flattify_movies(json_chunk):
     to_insert = [FlattenedMovie.create(movie_id) for movie_id in
                  Movie.objects(pk__in=json_chunk).values_list('data')]
-    FlattenedMovie.objects.insert(to_insert)
+    with transaction.atomic():
+        FlattenedMovie.objects.insert(to_insert)
     print("Persisted %s flattened movies" % len(to_insert))
 
 
@@ -24,8 +26,9 @@ def redo_movies_task(movie_ids):
     all_langs = dict([(lang.iso_639_1, lang) for lang in SpokenLanguage.objects.all()])
     all_countries = dict([(country.iso_3166_1, country) for country in ProductionCountries.objects.all()])
 
-    ids = [persist(x) for x in Movie.objects(pk__in=movie_ids)]
-    print("Redone %s movies with new structure" % len(ids))
+    with transaction.atomic():
+        ids = [persist(x) for x in Movie.objects(pk__in=movie_ids)]
+        print("Redone %s movies with new structure" % len(ids))
 
 
 @shared_task
@@ -42,8 +45,13 @@ def import_imdb_ratings_task(csv_rows_chunk):
             movies[movie[0]] = movie
 
         data = Movie.objects.filter(data__imdb_id__in=[csv_row[0] for csv_row in csv_rows_chunk])
+
         with transaction.atomic():
-            [make_entity(db, movies[db.data.imdb_id]).save() for db in data]
+            for movie in [make_entity(d, movies[d.data.imdb_id]) for d in data]:
+                Movie.objects(id=movie.id).update(set__data__imdb_vote_average=movie.data.imdb_vote_average,
+                                                  set__data__imdb_vote_count=movie.data.imdb_vote_count,
+                                                  set__data__weighted_rating=movie.data.weighted_rating)
+
     except Exception as e:
         __send_data_to_channel(layer=get_channel_layer(), message=f"Failed processing ratings for ids: {movies.keys()} "
                                                                   f"due to error: {e}")
@@ -56,16 +64,18 @@ def import_imdb_titles_task(chunk):
     chunked_map = dict()
     try:
         [chunked_map.setdefault(x[0], []).append({"alt_title": x[2], "iso": x[3]}) for x in chunk]
-        for fetched in Movie.objects.filter(data__imdb_id__in=list(chunked_map.keys())):
-            for alt in chunked_map.get(fetched.data.imdb_id):
-                iso = alt['iso']
-                title = alt['alt_title']
-                if not fetched.data.alternative_titles:
-                    fetched.data.alternative_titles = AlternativeTitles()
-                if iso != r'\N' and title not in fetched.data.alternative_titles.titles:
-                    fetched.data.alternative_titles.titles.append(Title(iso_3166_1=iso, title=title, type='IMDB'))
-            fetched.save()
+        with transaction.atomic():
+            for fetched in Movie.objects.filter(data__imdb_id__in=list(chunked_map.keys())):
+                for alt in chunked_map.get(fetched.data.imdb_id):
+                    iso = alt['iso']
+                    title = alt['alt_title']
+                    if not fetched.data.alternative_titles:
+                        fetched.data.alternative_titles = AlternativeTitles()
+                    if iso != r'\N' and title not in fetched.data.alternative_titles.titles:
+                        fetched.data.alternative_titles.titles.append(Title(iso_3166_1=iso, title=title, type='IMDB'))
+                fetched.save()
     except Exception as e:
-        __send_data_to_channel(layer=get_channel_layer(), message=f"Failed processing ratings for ids: {chunked_map.keys()} "
-                                                                  f"due to error: {e}")
+        __send_data_to_channel(layer=get_channel_layer(),
+                               message=f"Failed processing ratings for ids: {chunked_map.keys()} "
+                                       f"due to error: {e}")
     __send_data_to_channel(layer=get_channel_layer(), message=f"Processed {len(chunk)} titles")
