903165ba9ab3cb0dd687cf101c95455728c0f058
daveri <david.eriksson@avanza.se>
2024-04-08 16:39:08 +0200

    Fix port

--- features/environment.py
+++ features/environment.py
@@ -9,7 +9,7 @@ from testcontainers.mongodb import MongoDbContainer
 def before_all(context):
     with MongoDbContainer("mongo:7.0.7", port=29017, dbname="test") as mongo:
         os.environ['MONGO_URL'] = mongo.get_connection_client().HOST
-        os.environ['MONGO_PORT'] = str(mongo.get_connection_client().PORT)
+        os.environ['MONGO_PORT'] = str(29017)
     os.environ['TMDB_API'] = 'test'
     os.environ['ENVIRONMENT'] = 'test'
     settings.CELERY_TASK_EAGER_PROPAGATES = 'True'
